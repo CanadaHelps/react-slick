0c108047def2c048b70d5f047671dbf64652accd
Andrea Costantini <andreac@canadahelps.org>
2016-10-19 15:41:46 -0400

    Removed the warning about the slickGoTo prop.

--- src/inner-slider.js
+++ src/inner-slider.js
@@ -80,9 +80,6 @@ export var InnerSlider = React.createClass({
   },
   componentWillReceiveProps: function(nextProps) {
     if (this.props.slickGoTo != nextProps.slickGoTo) {
-      if (process.env.NODE_ENV !== 'production') {
-        console.warn('react-slick deprecation warning: slickGoTo prop is deprecated and it will be removed in next release. Use slickGoTo method instead')
-      }
       this.changeSlide({
           message: 'index',
           index: nextProps.slickGoTo,
